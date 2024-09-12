a6590118606b852771a940b7de64db1ef4e326dd
davfer12 <68206114+davfer12@users.noreply.github.com>
2024-09-12 19:08:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,68 +47,74 @@ figures.Figure_3(Figure_3_data)
 Figure_4_data = Eagle_sim
 figures.Figure_4(Figure_4_data)
 
-# Figure M1: Astrogeo catalogue pa distribution
+# Supplementary Figure 1: Astrogeo catalogue pa distribution
 Supplementary_Figure_1_data = Astrogeo_catalogue
-figures.Figure_M1(Supplementary_Figure_1_data)
+figures.Supplementary_Figure_1(Supplementary_Figure_1_data)
 
-# Figure M2: Comparison of DESI and Skymapper pa distribution
+# Supplementary Figure 2: Comparison of DESI and Skymapper pa distribution
 Supplementary_Figure_2_data = (DESI_xmatches, Skymapper_xmatches)
-figures.Figure_M2(Supplementary_Figure_2_data)
+figures.Supplementary_Figure_2(Supplementary_Figure_2_data)
 
-# Figure M3: Analysis with DESI data including magnitude tolerance
+# Supplementary Figure 3: Combined data analysis for various surveys
+N_bins = 5
+max_tol_Z_array = np.array([10, 0.5, 0.1])
+max_tol_err = 22.5
+Supplementary_Figure_3_data = (Surveys_Data, Surveys_Good_Data, max_tol_err, max_tol_Z_array, N_bins)
+figures.Supplementary_Figure_3(Supplementary_Figure_3_data)
+
+# Supplementary Figure 4: Analysis with SDSS data
+N_bins = 5
+max_tol_Z_array = np.array([10, 0.5, 0.1])
+max_tol_err = 22.5
+Supplementary_Figure_4_data = (Astrogeo_SDSS, SDSS_xmatches, max_tol_err, max_tol_Z_array, N_bins)
+figures.Supplementary_Figure_4(Supplementary_Figure_4_data)
+
+# Supplementary Figure 5: Analysis with DES data
+N_bins = 5
+max_tol_Z_array = np.array([10, 0.5, 0.1])
+max_tol_err = 22.5
+Supplementary_Figure_5_data = (Astrogeo_DES, DES_xmatches, max_tol_err, max_tol_Z_array, N_bins)
+figures.Supplementary_Figure_5(Supplementary_Figure_5_data)
+
+# Supplementary Figure 6: Analysis with SkyMapper data
+N_bins = 5
+max_tol_Z_array = np.array([10, 0.5, 0.1])
+max_tol_err = 22.5
+Supplementary_Figure_6_data = (Astrogeo_Skymapper, Skymapper_xmatches, max_tol_err, max_tol_Z_array, N_bins)
+figures.Supplementary_Figure_6(Supplementary_Figure_6_data)
+
+# Supplementary Figure 7: Analysis with DESI data and tolerance parameters for different bins
+N_bins = 10
+max_tol_Z_array = np.array([10, 0.5, 0.1])
+max_tol_err = 22.5
+Supplementary_Figure_7_data = (Astrogeo_DESI, DESI_xmatches, max_tol_err, max_tol_Z_array, N_bins)
+figures.Supplementary_Figure_7(Supplementary_Figure_7_data)
+
+
+# Supplementary Figure 8: Analysis with DESI data including magnitude tolerance
 N_bins = 5
 max_tol_Z_array = np.array([10, 0.5, 0.1])
 max_tol_err = 22.5
 max_mag_tol = 14
-Figure_M3_data = (Astrogeo_DESI, DESI_xmatches, max_tol_err, max_tol_Z_array, max_mag_tol, N_bins)
-figures.Figure_M3(Figure_M3_data)
+Supplementary_Figure_8_data = (Astrogeo_DESI, DESI_xmatches, max_tol_err, max_tol_Z_array, max_mag_tol, N_bins)
+figures.Supplementary_Figure_8(Supplementary_Figure_8_data)
 
-# Figure M4: Scatterplot and histogram of DESI pa vs. VLBI jet pa
-z_bound = 0.1
-Figure_M4_data = (Astrogeo_DESI, DESI_xmatches, z_bound)
-figures.Figure_M4(Figure_M4_data)
-
-# Figure M5: Scatterplot and histogram of DESI Delta pa vs. VLBI jet pa
-z_bound = 0.1
-Figure_M5_data = (Astrogeo_DESI, DESI_xmatches, z_bound)
-figures.Figure_M5(Figure_M5_data)
-
-# Figure M6: Analysis with DESI data and tolerance parameters for different bins
-N_bins = 10
-max_tol_Z_array = np.array([10, 0.5, 0.1])
-max_tol_err = 22.5
-Figure_M6_data = (Astrogeo_DESI, DESI_xmatches, max_tol_err, max_tol_Z_array, N_bins)
-figures.Figure_M6(Figure_M6_data)
-
-# Figure M7: Analysis of mean and median Delta pa with DESI data for redshift bins
+# Supplementary Figure 9: Analysis of mean and median Delta pa with DESI data for redshift bins
 z_tol = 1
-Figure_M7_data = (Astrogeo_DESI, DESI_xmatches, z_tol)
-figures.Figure_M7(Figure_M7_data)
+Supplementary_Figure_9_data = (Astrogeo_DESI, DESI_xmatches, z_tol)
+figures.Supplementary_Figure_9(Supplementary_Figure_9_data)
 
-# Figure M8: Combined data analysis for various surveys
-N_bins = 5
-max_tol_Z_array = np.array([10, 0.5, 0.1])
-max_tol_err = 22.5
-Figure_M8_data = (Surveys_Data, Surveys_Good_Data, max_tol_err, max_tol_Z_array, N_bins)
-figures.Figure_M8(Figure_M8_data)
+# Supplementary Figure 10: Scatterplot and histogram of DESI pa vs. VLBI jet pa
+z_bound = 0.1
+Supplementary_Figure_10_data = (Astrogeo_DESI, DESI_xmatches, z_bound)
+figures.Supplementary_Figure_10(Supplementary_Figure_10_data)
 
-# Figure M9: Analysis with SDSS data
-N_bins = 5
-max_tol_Z_array = np.array([10, 0.5, 0.1])
-max_tol_err = 22.5
-Figure_M9_data = (Astrogeo_SDSS, SDSS_xmatches, max_tol_err, max_tol_Z_array, N_bins)
-figures.Figure_M9(Figure_M9_data)
+# Supplementary Figure 11: Scatterplot and histogram of DESI Delta pa vs. VLBI jet pa
+z_bound = 0.1
+Supplementary_Figure_11_data = (Astrogeo_DESI, DESI_xmatches, z_bound)
+figures.Supplementary_Figure_11(Supplementary_Figure_11_data)
 
-# Figure M10: Analysis with DES data
-N_bins = 5
-max_tol_Z_array = np.array([10, 0.5, 0.1])
-max_tol_err = 22.5
-Figure_M10_data = (Astrogeo_DES, DES_xmatches, max_tol_err, max_tol_Z_array, N_bins)
-figures.Figure_M10(Figure_M10_data)
 
-# Figure M11: Analysis with SkyMapper data
-N_bins = 5
-max_tol_Z_array = np.array([10, 0.5, 0.1])
-max_tol_err = 22.5
-Figure_M11_data = (Astrogeo_Skymapper, Skymapper_xmatches, max_tol_err, max_tol_Z_array, N_bins)
-figures.Figure_M11(Figure_M11_data)
+
+
+
